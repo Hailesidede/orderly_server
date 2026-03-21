ce4190ebc9b3ba546fc96fc4268f901a8d588ae1
Haile Kesekwa <101603982+Hailesidede@users.noreply.github.com>
2026-03-22 00:25:00 +0300

    Update CorsConfig.java added import for corsconfiguration source

--- src/main/java/com/marketplace/backend/configs/CorsConfig.java
+++ src/main/java/com/marketplace/backend/configs/CorsConfig.java
@@ -3,6 +3,7 @@ package com.marketplace.backend.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
