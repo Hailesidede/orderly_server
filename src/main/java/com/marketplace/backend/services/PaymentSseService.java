3e32bdadafd335dddaa5c6743ec871ddee9755e8
Haile Kesekwa <101603982+Hailesidede@users.noreply.github.com>
2026-03-22 06:36:43 +0300

    Update PaymentSseService.java remved manual om

--- src/main/java/com/marketplace/backend/services/PaymentSseService.java
+++ src/main/java/com/marketplace/backend/services/PaymentSseService.java
@@ -17,10 +17,6 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor // FIX 3: Added the Lombok annotation so Spring injects your variables!
 public class PaymentSseService {
 
-    @Bean
-public com.fasterxml.jackson.databind.ObjectMapper objectMapper() {
-    return new com.fasterxml.jackson.databind.ObjectMapper();
-}
     private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
     
     private final StringRedisTemplate redisTemplate;
